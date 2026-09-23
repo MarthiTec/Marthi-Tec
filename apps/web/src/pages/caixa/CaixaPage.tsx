536503877dbcd49849925290f2e74657d76920f7
MarthiTec <marthi.tecnologia@gmail.com>
2026-09-23 14:13:30 -0300

    Caixa: múltiplas formas de pagamento e cálculo de troco

--- apps/web/src/pages/caixa/CaixaPage.tsx
+++ apps/web/src/pages/caixa/CaixaPage.tsx
@@ -1,1 +1,63 @@
-PLACEHOLDER_WILL_FAIL
+import { useEffect, useMemo, useRef, useState, type FormEvent } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { AdminIcon } from '../../components/AdminIcons';
+import { AdminPicker } from '../../components/AdminPicker';
+import { BrandLogo } from '../../components/BrandLogo';
+import { ExitOrLogoutDialog } from '../../components/ExitOrLogoutDialog';
+import { UserChip } from '../../components/UserChip';
+import { OperatorProfilePanel } from '../../components/OperatorProfilePanel';
+import { useAuth } from '../../contexts/AuthContext';
+import {
+  applyPriceTable,
+  attachOrderCashSession,
+  closePosSale,
+  findStockByCode,
+  findStockMatches,
+  getAdminState,
+  isWeighedUnit,
+  normalizeSaleQty,
+  stockItemImages,
+  type Customer,
+  type StockItem,
+  type StockUnit,
+} from '../../data/adminStore';
+import {
+  getOpenCashSession,
+  openCashDrawer,
+  registerCashSale,
+  type CashSession,
+} from '../../data/cashRegisterStore';
+import {
+  CASH_SETTINGS_EVENT,
+  getCashSettings,
+  readScaleKg,
+  verifyDeleteItemPassword,
+} from '../../data/cashSettings';
+import { listSellers, userIsStoreAdmin } from '../../data/erpRegistry';
+import {
+  applyTierTotal,
+  findCampaignsForStock,
+  PROMO_EVENT,
+} from '../../data/promoCampaignStore';
+import { emitNfeFromSale, emitSaleCheckoutDocument, FISCAL_KIND_LABEL } from '../../data/fiscalDocuments';
+import { hasDemoAccess } from '../../data/demoLeadStore';
+import { hasModule } from '../../data/storePlan';
+import { getTotemSettings } from '../../data/totemSettings';
+import { enqueueKitchenOrder } from '../../data/kitchenOrderStore';
+import { usePresenceSession } from '../../hooks/usePresence';
+import { usePanelTheme } from '../../hooks/usePanelTheme';
+import { CaixaPanelHost, type CaixaPanel } from './CaixaPanels';
+import { CaixaPaymentSplit } from './CaixaPaymentSplit';
+import {
+  createSplit,
+  describeSplit,
+  isVoucherPayment,
+  roundMoney,
+  summarizeSplit,
+  syncSingleSplit,
+  type SplitPayment,
+} from './paymentSplit';
+import '../admin/admin.css';
+import './caixa.css';
+
+const CONSUMIDOR_FINAL = 'Consumidor Final';
